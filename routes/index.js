debc4c9c21575af21f3c8adc71942fbe456e74a4
Ross Keeley <rosskeeley@jennifers-mbp.home>
2020-12-11 18:38:29 +0000

    added a header with an input field that takes an input when a button is clicked, and a list element with a button

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const app = express();
 var router = express.Router();
 
 /* GET home page. */
@@ -6,4 +7,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+app.get('/',(req, res) => {
+res.send('Hello World!');
+});
+
 module.exports = router;
+app.listen(3000, () => console.log("Server Up and running"));
